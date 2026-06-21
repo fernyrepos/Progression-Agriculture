834b4e84d615768f9383f52639547dd49d60a9ca
ferny <116464667+fernyrepos@users.noreply.github.com>
2026-06-21 03:09:47 -0400

    Revert "VPE Flower patch (#18)"
    This reverts commit b218420f4d3cd7a2828f9898a0ef78b924d273c1.

---
+++ 1.6/Source/PA_DefOf.cs
@@ -0,0 +1,17 @@
+using RimWorld;
+using Verse;
+
+namespace ProgressionAgriculture
+{
+    [DefOf]
+    public static class PA_DefOf
+    {
+        public static TraderKindDef AgriculturalTrader;
+        public static JobDef UnlockCrop;
+        public static ThingDef SeedPackingBench;
+        public static ThingDef SeedPackingCraftingSpot;
+        public static ThingCategoryDef SeedBundles;
+
+        public static ScenPartDef Ferny_StartingSeeds;
+    }
+}
