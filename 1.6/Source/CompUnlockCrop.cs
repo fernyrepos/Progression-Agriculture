2710a9c01a42e7a27abb04c09b99ec26203b7a5b
ferny <116464667+fernyrepos@users.noreply.github.com>
2026-08-12 17:18:39 -0400

    Open bundle gizmo

--- 1.6/Source/CompUnlockCrop.cs
+++ 1.6/Source/CompUnlockCrop.cs
@@ -1,6 +1,7 @@
 using Verse;
 using System.Collections.Generic;
 using RimWorld;
+using UnityEngine;
 using Verse.AI;
 
 namespace ProgressionAgriculture
@@ -12,16 +13,81 @@ namespace ProgressionAgriculture
 		{
 			if (GameComponent_UnlockedCrops.Instance.IsCropUnlocked(Props.cropDef) is false)
 			{
-				yield return new FloatMenuOption("PA.UnlockCrop".Translate(Props.cropDef.label), () =>
-				{
-					Job job = JobMaker.MakeJob(PA_DefOf.UnlockCrop, parent);
-					pawn.jobs.TryTakeOrderedJob(job);
-				});
+				yield return new FloatMenuOption("PA.UnlockCrop".Translate(Props.cropDef.label), () => QueueOpenJob(pawn));
 			}
 			else
 			{
 				yield return new FloatMenuOption("PA.CropAlreadyUnlocked".Translate(Props.cropDef.label).CapitalizeFirst(), null);
 			}
+		}
+
+		public override IEnumerable<Gizmo> CompGetGizmosExtra()
+		{
+			foreach (Gizmo gizmo in base.CompGetGizmosExtra())
+			{
+				yield return gizmo;
+			}
+
+			Command_Action command = new Command_Action
+			{
+				defaultLabel = "PA.UnlockCropGizmo".Translate(Props.cropDef.label),
+				defaultDesc = "PA.UnlockCropGizmoDesc".Translate(Props.cropDef.label),
+				icon = ContentFinder<Texture2D>.Get("SeedBundle"),
+				action = ShowPawnSelectionMenu
+			};
+
+			if (GameComponent_UnlockedCrops.Instance.IsCropUnlocked(Props.cropDef))
+			{
+				command.Disable("PA.CropAlreadyUnlocked".Translate(Props.cropDef.label).CapitalizeFirst());
+			}
+			else if (parent.Map == null)
+			{
+				command.Disable("PA.SeedBundleNotOnMap".Translate());
+			}
+
+			yield return command;
+		}
+
+		private void ShowPawnSelectionMenu()
+		{
+			List<FloatMenuOption> options = new List<FloatMenuOption>();
+			if (parent.Map == null)
+			{
+				options.Add(new FloatMenuOption("PA.SeedBundleNotOnMap".Translate(), null));
+				Find.WindowStack.Add(new FloatMenu(options));
+				return;
+			}
+
+			foreach (Pawn pawn in parent.Map.mapPawns.FreeColonistsSpawned)
+			{
+				Pawn localPawn = pawn;
+				string label = localPawn.LabelShortCap;
+				if (localPawn.Downed)
+				{
+					options.Add(new FloatMenuOption("PA.CannotOpenSeedBundleDowned".Translate(label), null));
+				}
+				else if (!localPawn.CanReserveAndReach(parent, PathEndMode.ClosestTouch, Danger.Deadly))
+				{
+					options.Add(new FloatMenuOption("PA.CannotOpenSeedBundleUnreachable".Translate(label), null));
+				}
+				else
+				{
+					options.Add(new FloatMenuOption(label, () => QueueOpenJob(localPawn)));
+				}
+			}
+
+			if (options.Count == 0)
+			{
+				options.Add(new FloatMenuOption("PA.NoColonistsCanOpenSeedBundle".Translate(), null));
+			}
+
+			Find.WindowStack.Add(new FloatMenu(options));
+		}
+
+		private void QueueOpenJob(Pawn pawn)
+		{
+			Job job = JobMaker.MakeJob(PA_DefOf.UnlockCrop, parent);
+			pawn.jobs.TryTakeOrderedJob(job);
 		}
 
 		public void Unlock()
